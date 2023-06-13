aefe2636d82ed09f42ad85567a2d76a7a5203396
ArielC13 <112133335+ArielC13@users.noreply.github.com>
2023-06-13 20:33:24 -0300

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,63 @@
+//console.log('Hola');
+
+// function miSuma(Numero1, Numero2){
+//    Suma =  Numero1 + Numero2
+//    return Suma;
+// }
+//console.log('El resultado de la suma es:', miSuma(25,30));
+
+var prompt = require('prompt-sync')({sigint:true});
+
+//N1 = parseInt(prompt('Ingrese numero '));
+//N2 = parseInt(prompt('Ingrese numero '));
+
+ //console.log('El resultado de la suma es: ', miSuma(N1,N2));
+
+ //var prompt = require('prompt-sync')({sigint:true});
+
+ /*Nombre = prompt('Ingrese su nombre: ');
+ Edad = prompt('Ingrese su edad: ');
+
+ if (Edad >= 18){
+    console.log('Usted es mayor de edad');
+ }
+ else{
+    console.log('Usted es menor de edad');
+ }
+*/
+ /*var Lista = [1,3,5,8,13,21];
+ Lista.forEach(function(x){
+    console.log(x);
+    console.log(miSuma(5,x));
+ });
+*/
+ var Lista2 = [];
+ for (var i = 0; i <= 10; i++){
+    Numeros = prompt('Ingrese un numero: ');
+    Lista2.push(Numeros);
+ }
+ console.log(Lista2);
+
+
+/* var Numero = 0;
+ while (Numero < 15){
+    Numero++;
+    console.log(Numero);
+ }
+
+ for (var i = 0; i < 15; i++){
+    console.log(i);
+ }
+ */
+
+
+
+
+ 
+
+
+
+
+
+
+
